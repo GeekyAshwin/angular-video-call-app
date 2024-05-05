57582e4d58aea48b70e5754f45f0d7d73459e651
Ashwiner <techmindashwin78@gmail.com>
2024-05-05 07:27:06 +0530

    added functionality to show remote user to sender

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ declare const Peer: any;
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   title = 'angular-video-call';
   public peer = new Peer();
   public currentUserPeerId: string = '';
@@ -27,24 +27,16 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('currentUserVideo') currentUserVideo: ElementRef | undefined;
   @ViewChild('remoteUserVideo') remoteUserVideo: ElementRef | undefined;
 
-  ngAfterViewInit(): void {
-    console.log(this.currentUserVideo, this.currentUserMediaStream);
-  }
 
   ngOnInit(): void {
     this.peer.on('open', (id: string) => {
       this.currentUserPeerId = id;
-      console.log('My peer ID is: ' + this.currentUserPeerId);
     });
     this.peer.on('call', (mediaConnection: any) => {
-      console.log('code to receve call');
 
       mediaConnection.answer(this.currentUserMediaStream);
-      console.log('answer called');
 
       mediaConnection.on('stream', (remoteMediaStream: any) => {
-        console.log('stream code here');
-        console.log(remoteMediaStream)
         navigator.mediaDevices
           .getUserMedia({ video: { height: 200, width: 200 }, audio: true })
           .then((mediaStream) => {
@@ -68,8 +60,17 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.remoteUserPeerId,
       this.currentUserMediaStream
     );
-    call.on('stream', function () {
-      console.log('code of stream');
+    call.on('stream', (remoteMediaStream: any) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: { height: 200, width: 200 }, audio: true })
+        .then((mediaStream) => {
+          this.remoteUserMediaStream = remoteMediaStream;
+          if (this.remoteUserVideo && this.remoteUserVideo.nativeElement) {
+            this.remoteUserVideo.nativeElement.srcObject = this.remoteUserMediaStream;
+          } else {
+            // this.currentUserVideo.nativeElement. = undefined;
+          }
+        });
       // `stream` is the MediaStream of the remote peer.
       // Here you'd add it to an HTML video/canvas element.
     });
